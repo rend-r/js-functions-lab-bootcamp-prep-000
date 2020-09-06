ca4ae261572bc6dbd51cb462acdf4fefb827047d
G G <georgiagray7@comcast.net>
2020-09-06 06:50:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // write your code below!
 function happyHolidays() {
-  return 'Happy holidays!'
+  return 'Happy holidays!';
 }
-function happyHolidaysTo('name') {
-  return console.log(`Happy holidays, ${name}!`)
+function happyHolidaysTo(name) {
+  return console.log(`Happy holidays, ${name}!`);
 }
-function happyCustomHolidayTo('holiday','name') {
-  return console.log(`Happy ${holiday}, ${name}!`)
+function happyCustomHolidayTo(holiday,name) {
+  return console.log(`Happy ${holiday}, ${name}!`);
 }
-function holidayCountdown('holiday', days) {
-  return console.log(`It's ${days} days until ${holiday}!`)
+function holidayCountdown(holiday, days) {
+  return console.log(`It's ${days} days until ${holiday}!`);
 }
