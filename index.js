6a9759d1ef540991bed2051c58979841bcbae092
G G <georgiagray7@comcast.net>
2020-09-06 06:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name) {
 function happyCustomHolidayTo(holiday,name) {
   console.log(`Happy ${holiday}, ${name}!`)
 }
+function holidayCountdown(days,holiday) {
+  console.log()
+}
