e997a817eb306bffead5808ad5a71c205b4f992a
G G <georgiagray7@comcast.net>
2020-09-06 06:41:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays() {
+  console.log("Happy holidays!")
+}
+function happyHolidaysTo() {
+  console.log(`Happy holidays, ${name}!`)
+}
+function happyCustomHolidayTo() {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
